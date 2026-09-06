4ed0e00c422031933fcb423e4ee4457df6b54e49
prohibits-none <saini.ritik7796@gmail.com>
2026-09-06 13:54:59 +0530

    Add global leaderboard configuration

---
+++ leaderboard-config.js
@@ -0,0 +1,7 @@
+// Space Shoot - Global Leaderboard configuration
+// Create a Supabase project, enable Anonymous Sign-Ins, run leaderboard.sql,
+// then paste your project URL and anon/public key below.
+window.SPACE_SHOOT_CONFIG = {
+    supabaseUrl: "YOUR_SUPABASE_PROJECT_URL",
+    supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY"
+};
